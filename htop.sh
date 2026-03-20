6e9e481a6398614d6dec3edd3d302b278692e6bb
nagaraj602 <264412729+nagaraj602@users.noreply.github.com>
2026-03-20 07:27:36 +0530

    Create htop.sh

---
+++ htop.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+distro=$(cat /etc/os-release | grep "^ID=" | cut -d "=" -f2 | sed 's/"//g')
+
+if [ "$distro" = "rhel" ]; then
+    sudo yum update -y > /dev/null
+    sudo dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm -y > /dev/null
+    sudo dnf install htop -y > /dev/null
+    htop
+elif [ "$distro" = "ubuntu" ]; then
+    sudo apt-get update -y > /dev/null
+    sudo apt install htop -y > /dev/null
+    htop
+else
+    echo "Unsupported Distribution - Only RHEL and Ubuntu supported."
+    exit 1
+fi
