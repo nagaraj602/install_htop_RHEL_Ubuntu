36acbc899add5f7bd59857f44e46d032af22eefa
nagaraj602 <264412729+nagaraj602@users.noreply.github.com>
2026-05-13 21:40:02 +0530

    Update htop.sh

--- htop.sh
+++ htop.sh
@@ -2,7 +2,7 @@
 
 distro=$(cat /etc/os-release | grep "^ID=" | cut -d "=" -f2 | sed 's/"//g')
 
-if [ "$distro" = "rhel" ]; then
+if [ "$distro" = "rhel" ] || [ "$distro" == "amzn" ]; then
     sudo yum update -y > /dev/null 2>&1
     sudo dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm -y > /dev/null 2>&1
     sudo dnf install htop -y > /dev/null 2>&1
